c45d9accd2548ba92312e3fbb7b7295238f865f5
Jordan <jordanleelong5@gmail.com>
2021-01-18 22:14:33 -0800

    extracted incidents iteration into function, added sesrch feature by title

--- src/components/BikeTracker.jsx
+++ src/components/BikeTracker.jsx
@@ -11,13 +11,26 @@ function BikeTracker() {
     useEffect(() => {
         fetch(url)
         .then(res => res.json())
-        // .then(res => console.log(res.incidents))
         .then(res => setIncidents(res.incidents))
     }, [])
 
     const handleChange = event => {
         setInput(event.target.value)
     }
+
+    const mapIncidentsToList = () => {
+        return filterByTitle().map(incident => {
+            return (
+                <li key={incident.id}>
+                    {incident.title}
+                </li>
+            )
+        })
+    }
+
+    const filterByTitle = () => {
+        return incidents.filter(incident => incident.title.includes(input));
+    };
     
 
     return (
@@ -28,7 +41,9 @@ function BikeTracker() {
                 <input type='text' id='search' value={input} onChange={handleChange} />
             </form>
         </div>
-        {incidents.map(incident => <li key={incident.id}>{incident.title}</li>)}
+        <ul>
+            {mapIncidentsToList()}
+        </ul>
         </section>
     )
 }
