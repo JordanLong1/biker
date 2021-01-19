7b90cdfd42e01b191e7e2ca0dc63afbb28ea3850
Jordan <jordanleelong5@gmail.com>
2021-01-18 21:55:16 -0800

    added controlled input

--- src/components/BikeTracker.jsx
+++ src/components/BikeTracker.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 function BikeTracker() {
 
     const [incidents, setIncidents] = useState([]); 
+    const [input, setInput] = useState(''); 
 
 
     const url = 'https://bikewise.org:443/api/v2/incidents?page=5&per_page=5&proximity=45.521728%2C-122.67326&proximity_square=100'
@@ -14,10 +15,19 @@ function BikeTracker() {
         .then(res => setIncidents(res.incidents))
     }, [])
 
+    const handleChange = event => {
+        setInput(event.target.value)
+    }
     
 
     return (
         <section>
+        <div>
+            <form>
+            <label htmlFor='search'>Bike Search</label>
+                <input type='text' id='search' value={input} onChange={handleChange} />
+            </form>
+        </div>
         {incidents.map(incident => <li key={incident.id}>{incident.title}</li>)}
         </section>
     )
