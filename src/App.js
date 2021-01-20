74287a5c0d85f0ad4ff9cdf4db27f92257662cb6
Jordan <jordanleelong5@gmail.com>
2021-01-19 23:45:03 -0800

    added footer with dummy text

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'; 
 import BikeTracker from './components/BikeTracker'
 import Navigation from './components/Navigation'
+import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
     <Navigation />
       <BikeTracker />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,41 @@
 import React from 'react'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
     return (
-        <div>
-            Footer
+        <div className='main-footer'>
+        <div className='container'>
+        <div className='row'>
+        <div className='col-md-3 col-sm-6'>
+        <h4>Dummy Text for now</h4>
+        <ul>
+            <li>dummy text</li>
+            <li>dummy text</li>
+            <li>dummy text</li>
+            <li>dummy text</li>
+        </ul>
+        </div>
+        <div className='col-md-3 col-sm-6'>
+        <h4>Dummy Text for now</h4>
+        <ul>
+            <li>dummy text</li>
+            <li>dummy text</li>
+            <li>dummy text</li>
+            <li>dummy text</li>
+        </ul>
+        </div>
+        <div className='col-md-3 col-sm-6'>
+        <h4>Dummy Text for now</h4>
+        <ul>
+            <li>dummy text</li>
+            <li>dummy text</li>
+            <li>dummy text</li>
+            <li>dummy text</li>
+        </ul>
+        </div>
+        </div>
+        </div>
+           
         </div>
     )
 }
