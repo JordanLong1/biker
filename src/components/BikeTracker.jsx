d717500c2603552cc623490ccb68665987197d2c
Jordan <jordanleelong5@gmail.com>
2021-01-25 13:49:52 -0800

    fixed all formatting

--- src/components/BikeTracker.jsx
+++ src/components/BikeTracker.jsx
@@ -1,60 +1,72 @@
-import React, {useState, useEffect} from 'react'; 
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState, useEffect } from 'react';
+import bikeImg from '../images/bike.jpg';
+
 function BikeTracker() {
+  const [incidents, setIncidents] = useState([]);
+  const [input, setInput] = useState('');
 
-    const [incidents, setIncidents] = useState([]); 
-    const [input, setInput] = useState(''); 
+  const url =
+    'https://bikewise.org:443/api/v2/incidents?page=5&per_page=5&proximity=45.521728%2C-122.67326&proximity_square=100';
 
+  useEffect(() => {
+    // eslint-disable-next-line no-undef
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => setIncidents(res.incidents));
+  }, []);
 
-    const url = 'https://bikewise.org:443/api/v2/incidents?page=5&per_page=5&proximity=45.521728%2C-122.67326&proximity_square=100'
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
 
-    useEffect(() => {
-        fetch(url)
-        .then(res => res.json())
-        .then(res => setIncidents(res.incidents))
-    }, [])
+  // eslint-disable-next-line no-use-before-define
+  const mapIncidentsToList = () =>
+    // eslint-disable-next-line no-use-before-define
+    filterByTitle().map((incident) => (
+      <section className="bike-card-row" key={incident.id}>
+        <div className="bike-detailed-row">
+          {incident.media.image_url === null ? (
+            <img src={bikeImg} alt="Default-Img" />
+          ) : (
+            <img src={incident.media.image_url} alt="Bike Img" />
+          )}
 
-    const handleChange = event => {
-        setInput(event.target.value)
-    }
+          <li className="incident-title">{incident.title} </li>
+          <li className="incident-description">
+            Description:
+            {incident.description}{' '}
+          </li>
 
-    const mapIncidentsToList = () => {
-        return filterByTitle().map(incident => {
-            console.log(incident)
-            return (
-                <section className='bike-card-row' key={incident.id}>
-                <div className='bike-detailed-row'>
-                    {incident.media.image_url === null ? <img src={'../images/bike.jpg'} alt='Default-Img'/> :
-                    <img src={incident.media.image_url} alt='Bike Img' />
-                     }
-                   
-                <li className='incident-title'>{incident.title} </li>
-                <li className='incident-description'>Description: {incident.description} </li>
-                <li className='incident-address'>Address: {incident.address} </li>
-                </div>
-                </section>
-            )
-        })
-    }
-
-    const filterByTitle = () => {
-        return incidents.filter(incident => incident.title.includes(input));
-    };
-    
-
-    return (
-        <section className='bike-info-display-container'>
-        <div>
-            <form>
-            <label htmlFor='search'>Bike Search</label>
-                <input type='text' id='search' value={input} onChange={handleChange} />
-                <button>Insert Search Logo here</button>
-            </form>
+          <li className="incident-address">
+            Address:
+            {incident.address}
+          </li>
         </div>
-        <ul>
-            {mapIncidentsToList()}
-        </ul>
-        </section>
-    )
+      </section>
+    ));
+
+  const filterByTitle = () =>
+    incidents.filter((incident) => incident.title.includes(input));
+
+  return (
+    <section className="bike-info-display-container">
+      <div>
+        <form>
+          <label htmlFor="search">Bike Search</label>
+          <input
+            type="text"
+            id="search"
+            value={input}
+            onChange={handleChange}
+          />
+          <button type="submit">Insert Search Logo here</button>
+        </form>
+      </div>
+      <ul>{mapIncidentsToList()}</ul>
+    </section>
+  );
 }
 
-export default BikeTracker
+export default BikeTracker;
