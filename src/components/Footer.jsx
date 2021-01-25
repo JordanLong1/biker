206745452f26195d7bcb76a4a251d787990c0df2
Jordan <jordanleelong5@gmail.com>
2021-01-25 13:49:27 -0800

    fixed formatting with prettier

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,47 @@
-import React from 'react'; 
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
-    return (
-        <div className='main-footer'>
-        <div className='container'>
-        <div className='row'>
-        <div className='col-md-3 col-sm-6'>
-        <h4>Dummy Text for now</h4>
-        <ul>
-            <li>dummy text</li>
-            <li>dummy text</li>
-            <li>dummy text</li>
-            <li>dummy text</li>
-        </ul>
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <h4>Dummy Text for now</h4>
+            <ul>
+              <li>dummy text</li>
+              <li>dummy text</li>
+              <li>dummy text</li>
+              <li>dummy text</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Dummy Text for now</h4>
+            <ul>
+              <li>dummy text</li>
+              <li>dummy text</li>
+              <li>dummy text</li>
+
+              <li>dummy text</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h4>Dummy Text for now</h4>
+            <ul>
+              <li>dummy text</li>
+              <li>dummy text</li>
+              <li>dummy text</li>
+              <li>dummy text</li>
+            </ul>
+          </div>
         </div>
-        <div className='col-md-3 col-sm-6'>
-        <h4>Dummy Text for now</h4>
-        <ul>
-            <li>dummy text</li>
-            <li>dummy text</li>
-            <li>dummy text</li>
-            <li>dummy text</li>
-        </ul>
-        </div>
-        <div className='col-md-3 col-sm-6'>
-        <h4>Dummy Text for now</h4>
-        <ul>
-            <li>dummy text</li>
-            <li>dummy text</li>
-            <li>dummy text</li>
-            <li>dummy text</li>
-        </ul>
-        </div>
-        </div>
-        </div>
-           <div className='privacy-policy-section'>
-               <p>Privacy Policy</p>
-               <p>2021 Copyright Biker</p>
-           </div>
-        </div>
-    )
+      </div>
+      <div className="privacy-policy-section">
+        <p>Privacy Policy</p>
+        <p>2021 Copyright Biker</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
