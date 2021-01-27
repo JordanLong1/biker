bd7479fd20184774dd699e2211ecc9fb583f2518
Jordan <jordanleelong5@gmail.com>
2021-01-26 22:34:29 -0800

    added another basic test

--- src/components/BikeTracker.test.js
+++ src/components/BikeTracker.test.js
@@ -3,8 +3,14 @@ import { render, screen, expect, getByLabelText } from '@testing-library/react';
 import React from 'react';
 import BikeTracker from './BikeTracker';
 
-test('it renders the component correctly', () => {
+test('it has a search button', () => {
   render(<BikeTracker />);
 
   screen.getByRole('button', { name: 'Insert Search Logo here' });
+});
+
+test('it has an input field', () => {
+  render(<BikeTracker />);
+
+  screen.getByRole('textbox', { name: 'Bike Search' });
 });
