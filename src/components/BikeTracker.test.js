efee14d3f08aca2ba0677d24e6b208a0db187973
Jordan <jordanleelong5@gmail.com>
2021-01-26 22:12:34 -0800

    changed original test, this is working.

--- src/components/BikeTracker.test.js
+++ src/components/BikeTracker.test.js
@@ -1,12 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
-import { render, screen, expect, test } from '@testing-library/react';
+import { render, screen, expect, getByLabelText } from '@testing-library/react';
 import React from 'react';
 import BikeTracker from './BikeTracker';
 
 test('it renders the component correctly', () => {
   render(<BikeTracker />);
 
-  const input = screen.getByLabelText('Bike Search');
-
-  expect(input).toHaveLength(1);
+  screen.getByRole('button', { name: 'Insert Search Logo here' });
 });
